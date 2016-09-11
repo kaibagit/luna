34ebced3a1b508c476b2122dca58f39bd980ce89
kaiba <luliruwy@163.com>
2016-09-11 23:17:36 +0800

    create model of framework

---
+++ luna-common/src/main/java/org/luna/common/persistent/Model.java
@@ -0,0 +1,47 @@
+package org.luna.common.persistent;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Date;
+
+/**
+ * Created by kaiba on 2016/9/11.
+ */
+public abstract class Model extends ActiveRecord {
+
+    @Id
+    @GeneratedValue
+    protected Integer id;
+    protected Date createTime;
+    protected Date updateTime;
+
+    public void save(){
+        validate();
+        this.doBeforeSave();
+        super.save();
+    }
+
+    public void update(){
+        validate();
+        this.doBeforeUpdate();
+        super.update();
+    }
+
+    public void doBeforeSave(){
+        Date now = new Date();
+        if(createTime == null)
+            createTime = now;
+        if(updateTime == null)
+            updateTime = now;
+    }
+
+    public void doBeforeUpdate(){
+        if(updateTime == null){
+            updateTime = new Date();
+        }
+    }
+
+    public void validate(){
+    }
+
+}
